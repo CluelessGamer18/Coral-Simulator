6c1dab60eadc301bf74e82c0d0cf1fcfa884b36d
danny446 <danieledwards513@gmail.com>
2026-03-13 16:42:13 -0600

    Commiting this so ashley has to do a merge

--- src/SimTutorial.jsx
+++ src/SimTutorial.jsx
@@ -1,15 +1,36 @@
+import { useState } from 'react';
+import './styles/SimTutorial.css';
 
 
 
-function SimTutorial(){
+function SimTutorial({closeTutorial=null}){
+    const [currentPage, setCurrentPage] = useState(1);
+    const lastPage = 3;
+
+    const nextPage = () => {
+        if(currentPage != lastPage){
+            setCurrentPage(currentPage + 1)
+        } else {
+            closeTutorial();
+        }
+    }
     return(
         <>
-        <div>
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
+        {(currentPage) == 1 ? 
+        <div className="SimTutorialPage">
+            <p> I am Page 1</p>
+            <button onClick={nextPage}>Next Page</button>
+        </div> : null}
+        {(currentPage) == 2 ? 
+        <div className="SimTutorialPage">
+            <p> I am Page 2</p>
+            <button onClick={nextPage}>Next Page</button>
+        </div> : null}
+        {(currentPage) == 3 ? 
+        <div className="SimTutorialPage">
+            <p> I am Page 3</p>
+            <button onClick={nextPage}>Next Page</button>
+        </div> : null}
         </>
     )
 }
